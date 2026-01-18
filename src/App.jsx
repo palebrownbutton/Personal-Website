377ae8693a7b899b233fe9e950f3dce70bfa3e02
palebrownbutton <simonavatcheva@hotmail.com>
2026-01-18 13:44:35 +0000

    Added title of the page
    Curved text with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,80 @@
 import { useState } from 'react'
 import './App.css'
 
-function backgroundImage() {
-  return "url('/src/assets/background.jpg')"
+function WelcomeText() {
+  return (
+    <svg width="800" height="700" viewBox="0 0 800 700">
+      <defs>
+        <linearGradient id="textGradientWelcome" x1="0%" y1="0%" x2="200%" y2="0%">
+          <stop offset="0%" stopColor="#095fab">
+            <animate attributeName="offset" values="-0.2;1.2" dur="4s" repeatCount="indefinite" />
+          </stop>
+          <stop offset="30%" stopColor="#25abe8">
+            <animate attributeName="offset" values="0.1;1.3" dur="4s" repeatCount="indefinite" />
+          </stop>
+          <stop offset="50%" stopColor="#4cd964">
+            <animate attributeName="offset" values="0;1" dur="4s" repeatCount="indefinite" />
+          </stop>
+          <stop offset="70%" stopColor="#25abe8">
+            <animate attributeName="offset" values="0.3;1.5" dur="4s" repeatCount="indefinite" />
+          </stop>
+          <stop offset="100%" stopColor="#095fab">
+            <animate attributeName="offset" values="0.8;1.8" dur="4s" repeatCount="indefinite" />
+          </stop>
+        </linearGradient>
+
+        <path id="arcPathWelcome" d="M50,350 A370,300 0 0,1 750,350" fill="transparent" />
+      </defs>
+
+      <text fontFamily="Indie Flower" fontSize="150" fill="url(#textGradientWelcome)">
+        <textPath xlinkHref="#arcPathWelcome" startOffset="50%" textAnchor="middle">
+          Welcome!
+        </textPath>
+      </text>
+    </svg>
+  )
+}
+
+function BuiltbyText() {
+  return (
+    <svg width="500" height="250" viewBox="0 0 500 250">
+      <defs>
+        <linearGradient id="textGradient" x1="0%" y1="0%" x2="200%" y2="0%">
+          <stop offset="0%" stopColor="#095fab">
+            <animate attributeName="offset" values="-0.2;1.2" dur="4s" repeatCount="indefinite" />
+          </stop>
+          <stop offset="30%" stopColor="#25abe8">
+            <animate attributeName="offset" values="0.1;1.3" dur="4s" repeatCount="indefinite" />
+          </stop>
+          <stop offset="50%" stopColor="#57d75b">
+            <animate attributeName="offset" values="-0.01;0.01" dur="4s" repeatCount="indefinite" />
+          </stop>
+          <stop offset="70%" stopColor="#25abe8">
+            <animate attributeName="offset" values="0.3;1.5" dur="4s" repeatCount="indefinite" />
+          </stop>
+          <stop offset="100%" stopColor="#095fab">
+            <animate attributeName="offset" values="0.8;1.8" dur="4s" repeatCount="indefinite" />
+          </stop>
+        </linearGradient>
+
+        <path id="arcPath" d="M50,280 A200,200 0 0,1 450,280" fill="transparent" />
+      </defs>
+
+      <text fontFamily="Indie Flower" fontSize="40" fill="url(#textGradient)">
+        <textPath xlinkHref="#arcPath" startOffset="50%" textAnchor="middle">
+          Built by Simona Vatcheva
+        </textPath>
+      </text>
+    </svg>
+  )
 }
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    <div>
-       <p className="header_font">Hello, World!</p>
+    <div className='hero'>
+      <WelcomeText />
+      <BuiltbyText />
     </div>
   )
 }
